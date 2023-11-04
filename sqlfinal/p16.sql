d8445c5ea66d3087088e0db37fb9ff787dc3e134
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2023-11-04 23:45:11 +0530

    Update p16.sql

--- sqlfinal/p16.sql
+++ sqlfinal/p16.sql
@@ -10,11 +10,12 @@ place, a record for the same is maintained in the increment_salary table.
 c) Write PL/SQL block using explicit cursor for following requirements: College has decided to
 mark all those students detained (D) who are having attendance less than 75%. Whenever such
 update takes place, a record for the same is maintained in the D_Stud table. create table
-stud21(roll number(4), att number(4), status varchar(1));In SQL, you typically use explicit cursors within PL/SQL blocks. Here are SQL blocks for the scenarios you described, using explicit cursors and including the creation of tables, data insertion, and cursor processing:
+stud21(roll number(4), att number(4), status varchar(1));
+Certainly, here are the SQL blocks with explicit cursors for each scenario, along with the creation of the tables and data insertion:
 
-a) Bank Account Activation (Implicit Cursor):
+1. Create the `account` table and insert sample data:
+
 ```sql
--- Create the account table and insert sample data
 CREATE TABLE account (
     account_id NUMBER,
     last_transaction_date DATE,
@@ -23,30 +24,11 @@ CREATE TABLE account (
 
 INSERT INTO account (account_id, last_transaction_date, status)
 VALUES (1, TO_DATE('2022-10-01', 'YYYY-MM-DD'), 'Inactive');
-
--- SQL block to activate inactive accounts and display a message
-UPDATE account
-SET status = 'Active'
-WHERE last_transaction_date < SYSDATE - 365;
-
--- Get the number of rows affected
-DECLARE
-    v_rows_affected NUMBER;
-BEGIN
-    SELECT COUNT(*) INTO v_rows_affected FROM account WHERE status = 'Active';
-
-    IF v_rows_affected > 0 THEN
-        DBMS_OUTPUT.PUT_LINE('Accounts updated: ' || v_rows_affected);
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('No accounts were updated.');
-    END IF;
-END;
-/
 ```
 
-b) Employee Salary Increase (Implicit Cursor):
+2. Create the `employee` table and insert sample data:
+
 ```sql
--- Create the employee table and insert sample data
 CREATE TABLE employee (
     employee_id NUMBER,
     salary NUMBER
@@ -54,31 +36,11 @@ CREATE TABLE employee (
 
 INSERT INTO employee (employee_id, salary)
 VALUES (1, 50000);
-
--- SQL block to increase salaries and maintain records
-DECLARE
-    v_avg_salary NUMBER;
-BEGIN
-    -- Calculate the average salary of the organization
-    SELECT AVG(salary) INTO v_avg_salary FROM employee;
-
-    -- Update salaries for employees with salary < average
-    UPDATE employee
-    SET salary = salary * 1.10
-    WHERE salary < v_avg_salary;
-
-    -- Insert records into increment_salary table (assuming you have one)
-    INSERT INTO increment_salary (employee_id, old_salary, new_salary)
-    SELECT employee_id, salary / 1.10, salary
-    FROM employee
-    WHERE salary < v_avg_salary;
-END;
-/
 ```
 
-c) Student Attendance (Implicit Cursor):
+3. Create the `stud21` table and insert sample data:
+
 ```sql
--- Create the stud21 table and insert sample data
 CREATE TABLE stud21 (
     roll NUMBER(4),
     att NUMBER(4),
@@ -90,23 +52,91 @@ VALUES (1, 80, 'A');
 
 INSERT INTO stud21 (roll, att, status)
 VALUES (2, 70, 'P');
+```
 
--- SQL block to mark detained students and maintain records
+Now, let's write SQL blocks with explicit cursors for each scenario:
+
+a) Bank Account Activation (Explicit Cursor):
+
+```sql
 DECLARE
-    v_attendance_threshold NUMBER := 75;
+    v_rows_affected NUMBER := 0;
+    v_account_id NUMBER;
+    CURSOR inactive_accounts_cursor IS
+        SELECT account_id
+        FROM account
+        WHERE last_transaction_date < (SYSDATE - 365);
 BEGIN
-    -- Update status to 'D' for students with attendance < 75%
-    UPDATE stud21
-    SET status = 'D'
-    WHERE att < v_attendance_threshold;
+    FOR account_rec IN inactive_accounts_cursor LOOP
+        v_account_id := account_rec.account_id;
 
-    -- Insert records into the D_Stud table (assuming you have one)
-    INSERT INTO D_Stud (roll, att)
-    SELECT roll, att
-    FROM stud21
-    WHERE status = 'D';
+        -- Update the status of inactive accounts
+        UPDATE account
+        SET status = 'Active'
+        WHERE account_id = v_account_id;
+
+        -- Increment the rows affected count
+        v_rows_affected := v_rows_affected + 1;
+    END;
+
+    IF v_rows_affected > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Accounts updated: ' || v_rows_affected);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('No accounts were updated.');
+    END IF;
 END;
 /
 ```
 
-Please note that the examples provided assume you have the necessary tables and structures in your database. You may need to adjust table and column names to match your actual schema.
+b) Employee Salary Increase (Explicit Cursor):
+
+```sql
+DECLARE
+    v_avg_salary NUMBER;
+    CURSOR eligible_employees_cursor IS
+        SELECT employee_id, salary
+        FROM employee
+        WHERE salary < (SELECT AVG(salary) FROM employee);
+BEGIN
+    -- Calculate the average salary of the organization
+    SELECT AVG(salary) INTO v_avg_salary FROM employee;
+
+    FOR employee_rec IN eligible_employees_cursor LOOP
+        -- Update salaries for employees with salary less than the average
+        UPDATE employee
+        SET salary = salary * 1.10
+        WHERE employee_id = employee_rec.employee_id;
+
+        -- Insert records into increment_salary table (assuming you have one)
+        INSERT INTO increment_salary (employee_id, old_salary, new_salary)
+        VALUES (employee_rec.employee_id, employee_rec.salary, employee_rec.salary * 1.10);
+    END;
+END;
+/
+```
+
+c) Student Attendance (Explicit Cursor):
+
+```sql
+DECLARE
+    v_attendance_threshold NUMBER := 75;
+    CURSOR detained_students_cursor IS
+        SELECT roll, att
+        FROM stud21
+        WHERE att < v_attendance_threshold;
+BEGIN
+    FOR student_rec IN detained_students_cursor LOOP
+        -- Update status to 'D' for students with attendance less than 75%
+        UPDATE stud21
+        SET status = 'D'
+        WHERE roll = student_rec.roll;
+
+        -- Insert records into the D_Stud table (assuming you have one)
+        INSERT INTO D_Stud (roll, att)
+        VALUES (student_rec.roll, student_rec.att);
+    END;
+END;
+/
+```
+
+You can run these SQL blocks in most relational database management systems (RDBMS) that support SQL. Make sure to adjust table and column names to match your actual schema.
