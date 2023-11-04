73193e7091e27e90ab25e3a11b50f1563984ca3a
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2023-11-04 18:38:11 +0530

    Update p8.sql

--- sqlfinal/p8.sql
+++ sqlfinal/p8.sql
@@ -15,90 +15,128 @@ more than the rate of pay of at least one employee with emp_no greater than or e
 9 .Get a list of employees not assigned a duty
 10.Get a count of different employees on each shift
 '''
-Certainly! Here are SQL queries for the specified questions based on the provided database:
+To solve these queries based on the given database structure, we will first create the tables, insert sample data, and then provide the SQL queries for each scenario:
 
-1. **Create Tables:**
+1. Create the tables and insert sample data:
 
 ```sql
--- Create the 'Employee' table
+-- Create the Employee table
 CREATE TABLE Employee (
     emp_no INT PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(100),
     skill VARCHAR(50),
     pay_rate DECIMAL(10, 2)
 );
 
--- Create the 'Position' table
+-- Insert sample data into the Employee table
+INSERT INTO Employee (emp_no, name, skill, pay_rate)
+VALUES
+    (123459, 'John', 'Chef', 25.00),
+    (123460, 'Alice', 'Chef', 27.00),
+    (123461, 'Bob', 'Waiter', 20.00),
+    (123462, 'Eva', 'Chef', 28.00),
+    (123463, 'Mia', 'Waiter', 21.00);
+
+-- Create the Position table
 CREATE TABLE Position (
     posting_no INT PRIMARY KEY,
     skill VARCHAR(50)
 );
 
--- Create the 'Duty_Allocation' table
+-- Insert sample data into the Position table
+INSERT INTO Position (posting_no, skill)
+VALUES
+    (1, 'Chef'),
+    (2, 'Waiter'),
+    (3, 'Manager'),
+    (4, 'Bartender');
+
+-- Create the Duty_Allocation table
 CREATE TABLE Duty_Allocation (
     posting_no INT,
     emp_no INT,
     day DATE,
-    shift VARCHAR(20)
+    shift INT
 );
+
+-- Insert sample data into the Duty_Allocation table
+INSERT INTO Duty_Allocation (posting_no, emp_no, day, shift)
+VALUES
+    (1, 123459, '1986-04-01', 1),
+    (1, 123459, '1986-04-02', 2),
+    (1, 123460, '1986-04-01', 1),
+    (2, 123461, '1986-04-01', 2),
+    (2, 123461, '1986-04-02', 1),
+    (2, 123462, '1986-04-01', 2),
+    (3, 123459, '1986-04-03', 1),
+    (3, 123460, '1986-04-03', 1),
+    (3, 123461, '1986-04-03', 2),
+    (4, 123459, '1986-04-04', 1);
+
+-- Insert more data for testing
+INSERT INTO Duty_Allocation (posting_no, emp_no, day, shift)
+VALUES
+    (1, 123459, '1986-04-03', 2),
+    (1, 123459, '1986-04-04', 1),
+    (1, 123460, '1986-04-03', 2),
+    (1, 123460, '1986-04-04', 1),
+    (1, 123461, '1986-04-04', 1);
 ```
 
-2. **Queries by number:**
+Now that you've created the tables and inserted sample data, you can proceed with the SQL queries:
 
-Query 1: Get the duty allocation details for `emp_no` 123461 for the month of April 1986.
+1. Get the duty allocation details for emp_no 123461 for the month of April 1986:
+
 ```sql
-SELECT *
+SELECT posting_no, day, shift
 FROM Duty_Allocation
-WHERE emp_no = 123461
-    AND MONTH(day) = 4
-    AND YEAR(day) = 1986;
+WHERE emp_no = 123461 AND DATEPART(YEAR, day) = 1986 AND DATEPART(MONTH, day) = 4;
 ```
 
-Query 2: Find the shift details for Employee 'xyz'.
+2. Find the shift details for Employee 'xyz':
+
 ```sql
-SELECT day, shift
+SELECT emp_no, day, shift
 FROM Duty_Allocation
 WHERE emp_no = (SELECT emp_no FROM Employee WHERE name = 'xyz');
 ```
 
-Query 3: Get employees whose rate of pay is more than or equal to the rate of pay of Employee 'xyz'.
+3. Get employees whose rate of pay is more than or equal to the rate of pay of employee 'xyz':
+
 ```sql
-SELECT name
+SELECT emp_no, name, pay_rate
 FROM Employee
 WHERE pay_rate >= (SELECT pay_rate FROM Employee WHERE name = 'xyz');
 ```
 
-Query 4: Get the names and pay rates of employees with `emp_no` less than 123460 whose rate of pay is more than the rate of pay of at least one employee with `emp_no` greater than or equal to 123460.
+4. Get the names and pay rates of employees with emp_no less than 123460 whose rate of pay is more than the rate of pay of at least one employee with emp_no greater than or equal to 123460:
+
 ```sql
 SELECT name, pay_rate
 FROM Employee
-WHERE emp_no < 123460
-    AND pay_rate > (SELECT MAX(pay_rate) FROM Employee WHERE emp_no >= 123460);
+WHERE emp_no < 123460 AND pay_rate > ANY (SELECT pay_rate FROM Employee WHERE emp_no >= 123460);
 ```
 
-Query 5: Find the names of employees who are assigned to all positions that require a Chef’s skill.
-```sql
-SELECT E.name
-FROM Employee AS E
-WHERE NOT EXISTS (
-    SELECT P.skill
-    FROM Position AS P
-    WHERE P.skill = 'Chef'
-    EXCEPT
-    SELECT D.skill
-    FROM Duty_Allocation AS D
-    WHERE D.emp_no = E.emp_no
-);
-```
+5. Find the names of employees who are assigned to all positions that require a Chef's skill:
 
-Query 6: Find the employees with the lowest pay rate.
 ```sql
 SELECT name
+FROM Employee
+WHERE skill = 'Chef'
+GROUP BY name
+HAVING COUNT(DISTINCT emp_no) = (SELECT COUNT(*) FROM Position WHERE skill = 'Chef');
+```
+
+6. Find the employees with the lowest pay rate:
+
+```sql
+SELECT emp_no, name, pay_rate
 FROM Employee
 WHERE pay_rate = (SELECT MIN(pay_rate) FROM Employee);
 ```
 
-Query 7: Get the employee numbers of all employees working on at least two dates.
+7. Get the employee numbers of all employees working on at least two dates:
+
 ```sql
 SELECT emp_no
 FROM Duty_Allocation
@@ -106,30 +144,29 @@ GROUP BY emp_no
 HAVING COUNT(DISTINCT day) >= 2;
 ```
 
-Query 8: Get a list of names of employees with the skill of Chef who are assigned a duty.
+8. Get a list of names of employees with the skill of Chef who are assigned a duty:
+
 ```sql
-SELECT E.name
-FROM Employee AS E
-WHERE E.skill = 'Chef'
-    AND E.emp_no IN (SELECT DISTINCT D.emp_no FROM Duty_Allocation AS D);
+SELECT DISTINCT E.name
+FROM Employee E
+INNER JOIN Duty_Allocation D ON E.emp_no = D.emp_no
+WHERE E.skill = 'Chef';
 ```
 
-Query 9: Get a list of employees not assigned a duty.
+9. Get a list of employees not assigned a duty:
+
 ```sql
-SELECT E.name
-FROM Employee AS E
-WHERE NOT EXISTS (
-    SELECT emp_no
-    FROM Duty_Allocation AS D
-    WHERE D.emp_no = E.emp_no
-);
+SELECT E.emp_no, E.name
+FROM Employee E
+WHERE E.emp_no NOT IN (SELECT DISTINCT emp_no FROM Duty_Allocation);
 ```
 
-Query 10: Get a count of different employees on each shift.
+10. Get a count of different employees on each shift:
+
 ```sql
 SELECT shift, COUNT(DISTINCT emp_no) AS EmployeeCount
 FROM Duty_Allocation
 GROUP BY shift;
 ```
 
-These SQL queries should help you retrieve the requested information from the database. Adjust the table and column names if they differ from the examples provided.
+You've now created tables, inserted data, and executed the SQL queries for the given scenarios.
