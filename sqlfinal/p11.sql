95b2f26e4cc05b265da0e6f1251088e079fca51c
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2023-11-04 18:43:12 +0530

    Update p11.sql

--- sqlfinal/p11.sql
+++ sqlfinal/p11.sql
@@ -14,122 +14,126 @@ e. Find the number of tuples in the customer relation
 f. Delete all the record of customers Sunil
 g. Create a view on deposit table.
 '''
-Here are the SQL table creations and queries based on your requirements:
+To solve these queries, we will first create the tables, add primary keys and foreign keys where applicable, insert sample data, and then provide the SQL queries for each scenario:
 
-1. **Create Tables with Primary and Foreign Keys:**
+1. Create the tables:
 
 ```sql
--- Create the 'Deposit' table
+-- Create the Deposit table
 CREATE TABLE Deposit (
     actno INT PRIMARY KEY,
-    cname VARCHAR(50),
-    bname VARCHAR(50),
+    cname VARCHAR(100),
+    bname VARCHAR(100),
     amount DECIMAL(10, 2),
-    adate DATE,
-    FOREIGN KEY (cname) REFERENCES Customers(cname),
-    FOREIGN KEY (bname) REFERENCES Branch(bname)
+    adate DATE
 );
 
--- Create the 'Branch' table
+-- Create the Branch table
 CREATE TABLE Branch (
-    bname VARCHAR(50) PRIMARY KEY,
-    city VARCHAR(50)
+    bname VARCHAR(100) PRIMARY KEY,
+    city VARCHAR(100)
 );
 
--- Create the 'Customers' table
+-- Create the Customers table
 CREATE TABLE Customers (
-    cname VARCHAR(50) PRIMARY KEY,
-    city VARCHAR(50)
+    cname VARCHAR(100) PRIMARY KEY,
+    city VARCHAR(100)
 );
 
--- Create the 'Borrow' table
+-- Create the Borrow table
 CREATE TABLE Borrow (
     loanno INT PRIMARY KEY,
-    cname VARCHAR(50),
-    bname VARCHAR(50),
+    cname VARCHAR(100),
+    bname VARCHAR(100),
     amount DECIMAL(10, 2),
     FOREIGN KEY (cname) REFERENCES Customers(cname),
     FOREIGN KEY (bname) REFERENCES Branch(bname)
 );
-```
 
-2. **Insert Data into the Tables:**
+-- Insert sample data into the tables
+INSERT INTO Deposit (actno, cname, bname, amount, adate)
+VALUES
+    (1, 'John', 'Perryridge', 4500.00, '1996-04-15'),
+    (2, 'Alice', 'Brighton', 6000.00, '1997-02-20'),
+    (3, 'Eva', 'Perryridge', 3800.00, '1996-05-05'),
+    (4, 'Bob', 'Brighton', 7000.00, '1997-01-25'),
+    (5, 'Anil', 'Brighton', 5500.00, '1997-03-15');
 
-```sql
--- Insert data into the 'Branch' table
 INSERT INTO Branch (bname, city)
 VALUES
     ('Perryridge', 'New York'),
-    ('Brighton', 'Bombay'),
-    ('Redwood', 'Pune');
+    ('Brighton', 'New York');
 
--- Insert data into the 'Customers' table
 INSERT INTO Customers (cname, city)
 VALUES
-    ('Anil', 'Pune'),
+    ('John', 'New York'),
+    ('Alice', 'New York'),
+    ('Eva', 'New York'),
     ('Bob', 'New York'),
-    ('Cathy', 'Bombay'),
-    ('Sunil', 'Pune');
+    ('Anil', 'New York');
 
--- Insert data into the 'Deposit' table
-INSERT INTO Deposit (actno, cname, bname, amount, adate)
+INSERT INTO Borrow (loanno, cname, bname, amount)
 VALUES
-    (1001, 'Anil', 'Perryridge', 5000.00, '1996-03-01'),
-    (1002, 'Anil', 'Redwood', 3500.00, '1996-04-15'),
-    (1003, 'Bob', 'Perryridge', 2000.00, '1997-01-12'),
-    (1004, 'Cathy', 'Brighton', 4500.00, '1996-11-05'),
-    (1005, 'Sunil', 'Pune', 3000.00, '1996-06-20');
-
--- Insert data into the 'Borrow' table (assuming data is available)
--- You can add data as needed for this table.
+    (101, 'John', 'Perryridge', 3000.00),
+    (102, 'Alice', 'Brighton', 4000.00),
+    (103, 'Eva', 'Perryridge', 2500.00),
+    (104, 'Bob', 'Brighton', 3500.00),
+    (105, 'Anil', 'Brighton', 2800.00);
 ```
 
-3. **Queries by letter:**
+Now that you've created the tables, added primary keys and foreign keys, and inserted sample data, you can proceed with the SQL queries:
 
-a. Display account date of customers Anil.
+a. Display the account date of customers named Anil:
+
 ```sql
 SELECT adate
 FROM Deposit
 WHERE cname = 'Anil';
 ```
 
-b. Modify the size of the 'amount' attribute in the 'Deposit' table.
-To modify the size of the 'amount' attribute, you can use an `ALTER TABLE` statement, but it depends on your specific database system. For example, in MySQL:
+b. Modify the size of the amount attribute in Deposit:
 
 ```sql
 ALTER TABLE Deposit
-MODIFY COLUMN amount DECIMAL(12, 2);
+ALTER COLUMN amount DECIMAL(12, 2);
 ```
 
-c. Display names of customers living in the city of Pune.
+c. Display names of customers living in the city 'Pune' (assuming 'Pune' is one of the city values):
+
 ```sql
 SELECT cname
 FROM Customers
 WHERE city = 'Pune';
 ```
 
-d. Display the name of the city where the branch 'KAROLBAGH' is located.
+d. Display the name of the city where the branch 'KAROLBAGH' is located:
+
 ```sql
 SELECT city
 FROM Branch
 WHERE bname = 'KAROLBAGH';
 ```
 
-e. Find the number of tuples (rows) in the 'Customers' table.
+e. Find the number of tuples (rows) in the Customers relation:
+
 ```sql
-SELECT COUNT(*) AS NumberOfTuples
+SELECT COUNT(*) AS TotalCustomers
 FROM Customers;
 ```
 
-f. Delete all the records of customers named Sunil.
+f. Delete all the records of customers named 'Sunil' (assuming 'Sunil' exists in the Customers table):
+
 ```sql
 DELETE FROM Customers
 WHERE cname = 'Sunil';
 ```
 
-g. Create a view on the 'Deposit' table.
+g. Create a view on the Deposit table:
+
 ```sql
 CREATE VIEW DepositView AS
 SELECT actno, cname, bname, amount, adate
 FROM Deposit;
 ```
+
+You've now created tables, inserted data, and executed the SQL queries for the given scenarios, including modifying the table structure and creating a view.
