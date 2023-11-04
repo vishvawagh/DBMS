66dd3bebc67d8643e6cc9b40b2afe74706f05fc5
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2023-11-04 18:36:27 +0530

    Update p7.sql

--- sqlfinal/p7.sql
+++ sqlfinal/p7.sql
@@ -11,89 +11,110 @@ Find the SQL queries for the following:
 5. Get employee numbers of employees who do not work on project C453
 '''
 
+To solve the queries based on the given database structure, we will first create the tables, insert sample data, and then provide the SQL queries for each scenario:
 
-1. **Create Tables:**
+1. Create the tables and insert sample data:
 
 ```sql
--- Create the 'Project' table
+-- Create the Project table
 CREATE TABLE Project (
     project_id INT PRIMARY KEY,
-    proj_name VARCHAR(50),
-    chief_arch INT,
-    FOREIGN KEY (chief_arch) REFERENCES Employee(Emp_id)
+    proj_name VARCHAR(100),
+    chief_arch INT
 );
 
--- Create the 'Employee' table
+-- Insert sample data into the Project table
+INSERT INTO Project (project_id, proj_name, chief_arch)
+VALUES
+    (C353, 'Project C353', 101),
+    (C354, 'Project C354', 102),
+    (DB, 'Database Project', 103);
+
+-- Create the Employee table
 CREATE TABLE Employee (
     Emp_id INT PRIMARY KEY,
-    Emp_name VARCHAR(50)
+    Emp_name VARCHAR(100)
 );
 
--- Create the 'Assigned-To' table
+-- Insert sample data into the Employee table
+INSERT INTO Employee (Emp_id, Emp_name)
+VALUES
+    (101, 'John Smith'),
+    (102, 'Alice Johnson'),
+    (103, 'Bob Davis'),
+    (104, 'Eva Wilson');
+
+-- Create the Assigned-To table
 CREATE TABLE Assigned_To (
     Project_id INT,
-    Emp_id INT,
-    FOREIGN KEY (Project_id) REFERENCES Project(project_id),
-    FOREIGN KEY (Emp_id) REFERENCES Employee(Emp_id)
+    Emp_id INT
 );
+
+-- Insert sample data into the Assigned-To table
+INSERT INTO Assigned_To (Project_id, Emp_id)
+VALUES
+    (C353, 101),
+    (C353, 102),
+    (C354, 102),
+    (DB, 103);
 ```
 
-2. **Queries by number:**
+Now that you've created the tables and inserted sample data, you can proceed with the SQL queries:
 
-Query 1: Get the details of employees working on project C353.
+1. Get the details of employees working on project C353:
+
 ```sql
-SELECT E.*
-FROM Employee AS E
-JOIN Assigned_To AS A ON E.Emp_id = A.Emp_id
-JOIN Project AS P ON A.Project_id = P.project_id
-WHERE P.proj_name = 'C353';
+SELECT E.Emp_id, E.Emp_name
+FROM Employee E
+INNER JOIN Assigned_To A ON E.Emp_id = A.Emp_id
+WHERE A.Project_id = 'C353';
 ```
 
-Query 2: Get employee numbers of employees working on project C353.
+2. Get employee numbers of employees working on project C353:
+
 ```sql
 SELECT E.Emp_id
-FROM Employee AS E
-JOIN Assigned_To AS A ON E.Emp_id = A.Emp_id
-JOIN Project AS P ON A.Project_id = P.project_id
-WHERE P.proj_name = 'C353';
+FROM Employee E
+INNER JOIN Assigned_To A ON E.Emp_id = A.Emp_id
+WHERE A.Project_id = 'C353';
 ```
 
-Query 3: Obtain details of employees working on the 'Database' project.
+3. Obtain details of employees working on the Database project:
+
 ```sql
-SELECT E.*
-FROM Employee AS E
-JOIN Assigned_To AS A ON E.Emp_id = A.Emp_id
-JOIN Project AS P ON A.Project_id = P.project_id
-WHERE P.proj_name = 'Database';
+SELECT E.Emp_id, E.Emp_name
+FROM Employee E
+INNER JOIN Assigned_To A ON E.Emp_id = A.Emp_id
+WHERE A.Project_id = 'DB';
 ```
 
-Query 4: Get details of employees working on both 'C353' and 'C354'.
+4. Get details of employees working on both C353 and C354:
+
 ```sql
-SELECT E.*
-FROM Employee AS E
+SELECT E.Emp_id, E.Emp_name
+FROM Employee E
 WHERE E.Emp_id IN (
-    SELECT A1.Emp_id
-    FROM Assigned_To AS A1
-    JOIN Project AS P1 ON A1.Project_id = P1.project_id
-    WHERE P1.proj_name = 'C353'
-) AND E.Emp_id IN (
-    SELECT A2.Emp_id
-    FROM Assigned_To AS A2
-    JOIN Project AS P2 ON A2.Project_id = P2.project_id
-    WHERE P2.proj_name = 'C354'
+    SELECT A.Emp_id
+    FROM Assigned_To A
+    WHERE A.Project_id = 'C353'
+)
+AND E.Emp_id IN (
+    SELECT A.Emp_id
+    FROM Assigned_To A
+    WHERE A.Project_id = 'C354'
 );
 ```
 
-Query 5: Get employee numbers of employees who do not work on project C453.
+5. Get employee numbers of employees who do not work on project C453:
+
 ```sql
 SELECT E.Emp_id
-FROM Employee AS E
+FROM Employee E
 WHERE E.Emp_id NOT IN (
     SELECT A.Emp_id
-    FROM Assigned_To AS A
-    JOIN Project AS P ON A.Project_id = P.project_id
-    WHERE P.proj_name = 'C453'
+    FROM Assigned_To A
+    WHERE A.Project_id = 'C453'
 );
 ```
 
-These SQL queries should help you retrieve the requested information from the database. Adjust the table and column names if they differ from the examples provided.
+You've now created tables, inserted data, and executed the SQL queries for the given scenarios.
