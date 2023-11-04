776f85e89f8245c414cd58153e447dd0d5c1aa9d
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2023-11-04 18:44:44 +0530

    Update p12.sql

--- sqlfinal/p12.sql
+++ sqlfinal/p12.sql
@@ -15,120 +15,126 @@ amount greater than 2000
 5. Display customer name living in the city where branch of depositor sunil is
 located.
 6. Create an index on deposit table'''
-Here are the SQL table creations and queries based on your requirements:
+To solve these queries, we will first create the tables, add primary keys and foreign keys where applicable, insert sample data, and then provide the SQL queries for each scenario:
 
-1. **Create Tables with Primary and Foreign Keys:**
+1. Create the tables:
 
 ```sql
--- Create the 'Deposit' table
+-- Create the Deposit table
 CREATE TABLE Deposit (
     actno INT PRIMARY KEY,
-    cname VARCHAR(50),
-    bname VARCHAR(50),
+    cname VARCHAR(100),
+    bname VARCHAR(100),
     amount DECIMAL(10, 2),
-    adate DATE,
-    FOREIGN KEY (cname) REFERENCES Customers(cname),
-    FOREIGN KEY (bname) REFERENCES Branch(bname)
+    adate DATE
 );
 
--- Create the 'Branch' table
+-- Create the Branch table
 CREATE TABLE Branch (
-    bname VARCHAR(50) PRIMARY KEY,
-    city VARCHAR(50)
+    bname VARCHAR(100) PRIMARY KEY,
+    city VARCHAR(100)
 );
 
--- Create the 'Customers' table
+-- Create the Customers table
 CREATE TABLE Customers (
-    cname VARCHAR(50) PRIMARY KEY,
-    city VARCHAR(50)
+    cname VARCHAR(100) PRIMARY KEY,
+    city VARCHAR(100)
 );
 
--- Create the 'Borrow' table
+-- Create the Borrow table
 CREATE TABLE Borrow (
     loanno INT PRIMARY KEY,
-    cname VARCHAR(50),
-    bname VARCHAR(50),
+    cname VARCHAR(100),
+    bname VARCHAR(100),
     amount DECIMAL(10, 2),
     FOREIGN KEY (cname) REFERENCES Customers(cname),
     FOREIGN KEY (bname) REFERENCES Branch(bname)
 );
-```
 
-2. **Insert Data into the Tables:**
+-- Insert sample data into the tables
+INSERT INTO Deposit (actno, cname, bname, amount, adate)
+VALUES
+    (1, 'John', 'Perryridge', 4500.00, '1996-04-15'),
+    (2, 'Alice', 'Brighton', 6000.00, '1997-02-20'),
+    (3, 'Eva', 'Perryridge', 3800.00, '1996-05-05'),
+    (4, 'Bob', 'Brighton', 7000.00, '1997-01-25'),
+    (5, 'Anil', 'Brighton', 5500.00, '1997-03-15');
 
-```sql
--- Insert data into the 'Branch' table
 INSERT INTO Branch (bname, city)
 VALUES
     ('Perryridge', 'New York'),
-    ('Brighton', 'Bombay'),
-    ('Redwood', 'Pune'),
-    ('Nagpur', 'Nagpur');
+    ('Brighton', 'New York');
 
--- Insert data into the 'Customers' table
 INSERT INTO Customers (cname, city)
 VALUES
-    ('Anil', 'Pune'),
+    ('John', 'New York'),
+    ('Alice', 'New York'),
+    ('Eva', 'New York'),
     ('Bob', 'New York'),
-    ('Cathy', 'Bombay'),
-    ('Sunil', 'Nagpur');
+    ('Anil', 'New York');
 
--- Insert data into the 'Deposit' table
-INSERT INTO Deposit (actno, cname, bname, amount, adate)
+INSERT INTO Borrow (loanno, cname, bname, amount)
 VALUES
-    (1001, 'Anil', 'Perryridge', 5000.00, '1996-03-01'),
-    (1002, 'Anil', 'Redwood', 3500.00, '1996-04-15'),
-    (1003, 'Bob', 'Perryridge', 2000.00, '1997-01-12'),
-    (1004, 'Cathy', 'Brighton', 4500.00, '1996-11-05'),
-    (1005, 'Sunil', 'Nagpur', 3000.00, '1996-06-20');
-
--- Insert data into the 'Borrow' table (assuming data is available)
--- You can add data as needed for this table.
+    (101, 'John', 'Perryridge', 3000.00),
+    (102, 'Alice', 'Brighton', 4000.00),
+    (103, 'Eva', 'Perryridge', 2500.00),
+    (104, 'Bob', 'Brighton', 3500.00),
+    (105, 'Anil', 'Brighton', 2800.00);
 ```
 
-3. **Queries by number:**
+Now that you've created the tables, added primary keys and foreign keys, and inserted sample data, you can proceed with the SQL queries:
 
-Query 1: Display customer names living in Bombay and having their branch city as Nagpur.
+1. Display customer names living in the city 'Bombay' and branch city 'Nagpur':
+
 ```sql
-SELECT c.cname
-FROM Customers AS c
-JOIN Branch AS b ON c.city = 'Bombay' AND b.city = 'Nagpur' AND c.cname = b.bname;
+SELECT C.cname
+FROM Customers C
+JOIN Branch B ON C.city = 'Bombay' AND B.city = 'Nagpur';
 ```
 
-Query 2: Display customer names having the same living city as their branch city.
+2. Display customer names having the same living city as their branch city:
+
 ```sql
-SELECT c.cname
-FROM Customers AS c
-JOIN Branch AS b ON c.city = b.city AND c.cname = b.bname;
+SELECT C.cname
+FROM Customers C
+JOIN Branch B ON C.city = B.city;
 ```
 
-Query 3: Display customer names who are both borrowers and depositors and live in Nagpur.
+3. Display customer names who are both borrowers and depositors and have a living city of 'Nagpur':
+
 ```sql
-SELECT DISTINCT c.cname
-FROM Customers AS c
-WHERE c.cname IN (SELECT cname FROM Borrow)
-AND c.cname IN (SELECT cname FROM Deposit)
-AND c.city = 'Nagpur';
+SELECT DISTINCT C.cname
+FROM Customers C
+JOIN Deposit D ON C.cname = D.cname
+JOIN Borrow B ON C.cname = B.cname
+WHERE C.city = 'Nagpur';
 ```
 
-Query 4: Display borrower names having deposit amounts greater than 1000 and loan amounts greater than 2000.
+4. Display borrower names having a deposit amount greater than 1000 and a loan amount greater than 2000:
+
 ```sql
-SELECT DISTINCT bo.cname
-FROM Borrow AS bo
-JOIN Deposit AS de ON bo.cname = de.cname
-WHERE de.amount > 1000
-AND bo.amount > 2000;
+SELECT DISTINCT B.cname
+FROM Borrow B
+WHERE B.amount > 2000
+AND B.cname IN (
+    SELECT D.cname
+    FROM Deposit D
+    WHERE D.amount > 1000
+);
 ```
 
-Query 5: Display customer names living in the city where the branch of depositor 'Sunil' is located.
+5. Display customer names living in the city where the branch of depositor 'Sunil' is located:
+
 ```sql
-SELECT c.cname
-FROM Customers AS c
-JOIN Deposit AS d ON c.city = (SELECT city FROM Branch WHERE bname = d.bname AND c.cname = 'Sunil');
+SELECT C.cname
+FROM Customers C
+WHERE C.city = (SELECT B.city FROM Branch B WHERE B.bname = (SELECT D.bname FROM Deposit D WHERE D.cname = 'Sunil'));
 ```
 
-Query 6: Create an index on the 'Deposit' table.
+6. Create an index on the Deposit table:
+
 ```sql
 CREATE INDEX idx_amount ON Deposit(amount);
 ```
 
+You've now created tables, inserted data, and executed the SQL queries for the given scenarios, including creating an index.
